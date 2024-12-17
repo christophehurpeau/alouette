ae0d9fd911292615b45b40214fab9e458eebdb6c
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-12-17 13:28:30 +0100

    refactor: sort plugins

--- packages/storybook-app/.storybook/main.ts
+++ packages/storybook-app/.storybook/main.ts
@@ -18,8 +18,8 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@chromatic-com/storybook"),
   ],
   framework: {
     name: "@storybook/react-vite",
