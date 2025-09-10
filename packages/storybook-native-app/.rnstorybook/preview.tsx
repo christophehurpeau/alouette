976b8c19bee2cf40a2f3bc5b4751c05791209924
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-09-10 20:58:09 +0200

    chore: update storybook

--- packages/storybook-native-app/.rnstorybook/preview.tsx
+++ packages/storybook-native-app/.rnstorybook/preview.tsx
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react-native";
 import { AlouetteDecorator } from "alouette";
 import tamaguiConfig from "../tamagui.config";
 
