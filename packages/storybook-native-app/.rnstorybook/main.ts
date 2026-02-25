b1bf20f4c9bd821f12c33c05712576d7fe6424ec
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-02-25 21:46:05 +0100

    fix: add autogenerated storybook.requires.ts

--- packages/storybook-native-app/.rnstorybook/main.ts
+++ packages/storybook-native-app/.rnstorybook/main.ts
@@ -5,15 +5,11 @@ import type { StorybookConfig } from "@storybook/react-native";
 const main: StorybookConfig = {
   stories: ["../../alouette/src/**/*.stories.@(ts|tsx)"],
   addons: [
-    getAbsolutePath("@storybook/addon-ondevice-notes"),
-    getAbsolutePath("@storybook/addon-ondevice-controls"),
-    getAbsolutePath("@storybook/addon-ondevice-backgrounds"),
-    getAbsolutePath("@storybook/addon-ondevice-actions"),
+    "@storybook/addon-ondevice-notes",
+    "@storybook/addon-ondevice-controls",
+    "@storybook/addon-ondevice-backgrounds",
+    "@storybook/addon-ondevice-actions",
   ],
 };
 
 export default main;
-
-function getAbsolutePath(value: string): any {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
-}
