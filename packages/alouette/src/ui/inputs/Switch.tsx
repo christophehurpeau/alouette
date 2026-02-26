6ff9475d91f46c31d5de88493a015859307f39fc
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-02-26 22:14:43 +0100

    feat: improve switch render on native

--- packages/alouette/src/ui/inputs/Switch.tsx
+++ packages/alouette/src/ui/inputs/Switch.tsx
@@ -1,17 +1,50 @@
-// import type { ThemeProps } from "@tamagui/core";
-import type { ComponentProps } from "react";
+import { Theme, type ThemeName, useTheme } from "@tamagui/core";
+import type { ComponentProps, ReactNode } from "react";
 import { Switch as NativeSwitch } from "react-native";
 
-export function Switch({
-  // checkedTheme,
-  checked,
-  ...rest
-}: Pick<
+export type SwitchProps = Pick<
   ComponentProps<typeof NativeSwitch>,
   "aria-labelledby" | "disabled" | "onValueChange"
 > & {
+  theme?: ThemeName;
   checked?: boolean;
-  // checkedTheme?: ThemeProps["name"];
-}) {
-  return <NativeSwitch value={checked} {...rest} />;
+  // checkedTheme?: ThemeName;
+};
+function ThemedNativeSwitch({
+  checked,
+  ...rest
+}: Omit<SwitchProps, "theme">): ReactNode {
+  const theme = useTheme();
+  const backgroundColor = rest.disabled
+    ? theme["$interactive.forms.backgroundColor:disabled"]?.get()
+    : theme["$bg-lowered"]?.get();
+
+  const thumbColor = rest.disabled
+    ? theme["$text-disabled-muted"]?.get()
+    : theme["$bg-highlight"]?.get();
+
+  return (
+    <NativeSwitch
+      ios_backgroundColor={backgroundColor}
+      trackColor={{
+        false: backgroundColor,
+        true: backgroundColor,
+      }}
+      thumbColor={thumbColor}
+      value={checked}
+      {...rest}
+    />
+  );
+}
+
+export function Switch({
+  // checkedTheme,
+  theme,
+  ...rest
+}: SwitchProps): ReactNode {
+  return (
+    <Theme name={theme}>
+      <ThemedNativeSwitch {...rest} />
+    </Theme>
+  );
 }
--- packages/storybook-native-app/babel.config.js
+++ packages/storybook-native-app/babel.config.js
@@ -2,5 +2,6 @@ export default function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { reanimated: false }]],
+    plugins: ["react-native-worklets/plugin"],
   };
 }
